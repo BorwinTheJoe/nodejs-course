0434ac4eef52e9c9dbad294c5afd08d93f0cc957
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-23 15:18:48 +0100

    Custom Module creation and export

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 console.log('Hello World');
-console.log(global);
+//console.log(global);
 
 //Course Link https://www.youtube.com/watch?v=JZXQ455OT3A&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw
 
 const os = require('os');
 const path = require('path');
+const {add, subtract, multiply, divide} = require('./math');
 
+/*
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
@@ -18,3 +20,9 @@ console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 
 console.log(path.parse(__filename));
+*/
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
