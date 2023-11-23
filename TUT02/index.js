1fa038ee0eaf9dc172984b1f83de469f77e59950
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-23 15:34:07 +0100

    Read, Write and Append file

---
+++ TUT02/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+//Read file and Console.log Hello happen at the same time, but read file takes longer.
+//Adding the encoding as utf8 so that we acquire data in text form. Could instead use parse.string for data.
+//path.join to create the path to the file rather than write it in text. This way it works no matter the operating system.
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+})
+
+console.log('Hello...');
+
+
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to Meet you!', (err) => {
+  if (err) throw err;
+  console.log('Write Complete');
+
+  fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it Is!', (err) => {
+    if (err) throw err;
+    console.log('Append Complete');
+  })
+})
+
+
+
+// exit on uncaught errors
+process.on('uncaughtException', err => {
+  console.error('There was an uncaught error: ${err}`')
+  process.exit(1);
+})
