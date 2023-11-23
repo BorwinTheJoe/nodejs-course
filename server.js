1de904357bce5d07d407aeffb9201b5b532eb0cf
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-23 15:12:31 +0100

    Started NodeJs Course start

---
+++ server.js
@@ -0,0 +1,20 @@
+console.log('Hello World');
+console.log(global);
+
+//Course Link https://www.youtube.com/watch?v=JZXQ455OT3A&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw
+
+const os = require('os');
+const path = require('path');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
