76de720afcd41ff7fda9db1c095f70124bb60dad
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-24 02:30:23 +0100

    Using Regular Expressions for better responses. Added comments for RegEx use. Added response Redirect

--- TUT06/server.js
+++ TUT06/server.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
-
-app.get('^/$|/index.html', (req, res) => {
+//when the app.get url request is:
+// ^/$ = Just the forward slash
+// | = OR
+// index(.html)?
+// where (.html)? means .html is Optional.
+app.get('^/$|/index(.html)?', (req, res) => {
     console.log(req.url, req.method);
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
     console.log(req.url, req.method);
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    console.log(req.url, req.method);
+    res.redirect(301, '/new-page.html'); // 302 code redirect by default. we want a 301. which is a permanent re-dir.
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
