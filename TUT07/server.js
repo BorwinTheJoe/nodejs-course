a7e04595c8e8c22afacf21980f305be9d33d470e
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-26 16:20:16 +0100

    Added logEvents module method to middleware

--- TUT07/server.js
+++ TUT07/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const logEvents = require('./middleware/logEvents');
 const PORT = process.env.PORT || 8080;
 
 // Custom-made middleware logger.
 app.use((req, res, next) => {
-    console.log(`${request.method} ${req.path}`);
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method} ${req.path}`);
     next();
 });
 
