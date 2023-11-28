0ccceb7533fe39b521f940a3a459ad7f02e1b92e
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-11-28 21:03:22 +0100

    Creating routers. Creating a route into the subdir folder. Added a static handler for the subdirectory. App.use can use Regex in newer versions of express. Creating root router. Server.js cleanup by separating routers out of it. Removing unnecessary code.

--- TUT08/server.js
+++ TUT08/server.js
@@ -26,61 +26,19 @@ app.use(cors(corsOptions));
 // built-in middleware to handle url-encoded data. 
 // Like form data.
 app.use(express.urlencoded({ extended: false }));
+
 // built-in middleware for serving json files.
 app.use(express.json());
+
 // serve static files
 // (will route into public folder to search for those files)
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-// * is for Anything.
-app.get('/*', (req, res, next) => {
-    console.log(req.url, req.method);
-    next();
-});
-// When the app.get url request is:
-// ^/$ = Just the forward slash
-// | = OR
-// index(.html)?
-// where (.html)? means .html is Optional.
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+//Routes any subdirectory request to the routes subdir.
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    // 302 code redirect by default. we want a 301. which is a permanent re-dir.
-    res.redirect(301, '/new-page.html'); 
-});
-
-// Route Handlers!
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next(); // Allows us to chain responses! it goes to next func in chain
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-};
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-};
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-};
-
-app.get('/chain(.html)?', [one, two, three]);
 
 // app.use('/'); DOES NOT USE REGEX!!
 // using * means that ANYTHING that gets here gets the 404.html
