8e4529c5e94f29de041fddd14e4eb0fda52c30d4
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-12-06 15:10:31 +0100

    Found issue with .env DATABASE URI reference. .env had DATABSE_URI instead of DATABASE_URI.

--- TUT13/config/dbConn.js
+++ TUT13/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mongotut:testing123@cluster0.irmtae7.mongodb.net/?retryWrites=true&w=majority", { 
+        await mongoose.connect(process.env.DATABASE_URI, { 
             // process.env.DATABASE_URI Doesn't work?? .env connection wrong??
             // Have to use the path itself. . .
         });
