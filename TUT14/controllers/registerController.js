4ec34c274ecd2502206b91bfe6d2c0a9b76def08
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-12-07 19:20:42 +0100

    Modifying Controllers to use Mongoose models.
    Modified registerController to handle user creation with MongoseDB.
    Tested registerController with Thunder Client.
    Checked Data on mongooseDB site.
    Modified one user to have Admin and Editor Roles.

--- TUT14/controllers/registerController.js
+++ TUT14/controllers/registerController.js
@@ -1,36 +1,38 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUser: function (data) { this.users = data }
-};
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': "Username and Password are required."});
+    if (!user || !pwd)
+        return res
+            .status(400)
+            .json({ message: 'Username and Password are required.' });
+
     // Check for duplicate usernames in the database.
-    const duplicate = usersDB.users.find(person => person.username === user);
+    const duplicate = await User.findOne({ username: user }).exec();
     if (duplicate) return res.sendStatus(409); // HTTP code for Conflict
+
     try {
         // Encrypt the Password
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        // Store the new User
-        const newUser = { 
-            "username": user, 
-            "roles": { "User": 2001 },
-            "password": hashedPwd 
-        };
-        usersDB.setUser([...usersDB.users, newUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
-        res.status(201).json({ 'message': `New User ${user} created!`});
+        // Create and Store the new User
+        const result = await User.create({
+            username: user,
+            password: hashedPwd
+        });
+
+        /* // Can also do it This way.
+        const newUser = new User();
+        newUser.username = user;
+        newUser.password = hashedPwd;
+        const result = await newUser.save();
+        */
+
+        console.log(result);
+        res.status(201).json({ message: `New User "${user}" created!` });
     } catch (err) {
-        res.status(500).json({ 'message': err.message });
+        res.status(500).json({ message: err.message });
     }
-}
+};
 
 module.exports = { handleNewUser };
