ab6e1f1bc1c501f73f8b4b936010dee80bfc736d
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2023-12-08 19:36:21 +0100

    Modified auth controller to handle refreshToken Array. Added refreshToken clearing.

--- TUT15/controllers/authController.js
+++ TUT15/controllers/authController.js
@@ -3,11 +3,9 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
+    const cookies = req.cookies;
     const { user, pwd } = req.body;
-    if (!user || !pwd)
-        return res
-            .status(400)
-            .json({ message: 'Username and Password are required.' });
+    if (!user || !pwd) return res.status(400).json({ message: 'Username and Password are required.' });
 
     const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401); // Unauthorized
@@ -27,19 +25,25 @@ const handleLogin = async (req, res) => {
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
-        const refreshToken = jwt.sign(
-            { username: foundUser.username },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '1d' }
-        );
+        const newRefreshToken = jwt.sign({ username: foundUser.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+
+        const newRefreshTokenArray = !cookies?.jwt ? foundUser.refreshToken : foundUser.refreshToken.filter((rt) => rt !== cookies.jwt);
+
+        if (cookies?.jwt) {
+            res.clearCookie('jwt', {
+                httpOnly: true,
+                sameSite: 'None',
+                secure: true
+            });
+        }
 
         // Saving refreshToken with current User. Later, Connect to database
-        foundUser.refreshToken = refreshToken;
+        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
         const result = await foundUser.save();
         console.log(result);
 
         //We send the cookie with the Refresh token as httpOnly as it's inaccessible with js!
-        res.cookie('jwt', refreshToken, {
+        res.cookie('jwt', newRefreshToken, {
             httpOnly: true,
             sameSite: 'None',
             secure: true,
